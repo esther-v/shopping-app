6e8e4913d00f17cf2ce4c134aed131607fb2fb86
esther-v <esvalas@gmail.com>
2024-01-31 18:27:31 +0100

    add routes for navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { Routes, Route } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
+import Home from './pages/Home'
+import Store from './pages/Store'
+import About from './pages/About'
+
+
 function App() {
 
   return (
-    <h1>Hello world</h1>
+    <Container>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/store" element={<Store />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Container>
   )
 }
 
