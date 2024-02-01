24bce35c5a611736ee04e741e0c03a584585ee70
esther-v <esvalas@gmail.com>
2024-02-01 20:10:50 +0100

    add logo and icon cart

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,23 @@
-import { Container, Nav, Navbar as NavbarBs } from 'react-bootstrap'
+import { Button, Container, Nav, Navbar as NavbarBs } from 'react-bootstrap'
 import { NavLink } from 'react-router-dom'
+import Logo from '../assets/C.png'
+
 
 function Navbar() {
   return (
     <NavbarBs className="bg-white shadow-sm mb-3">
       <Container>
-        <Nav>
-          <Nav.Link to="/" as={NavLink}>Home</Nav.Link>
-        </Nav>
-        <Nav>
+        <Nav className='me-auto'>
+          <Nav.Link to="/" as={NavLink}><img style={{ width: "3rem", height: "3rem"}} src={Logo}/></Nav.Link>
           <Nav.Link to="/store" as={NavLink}>Store</Nav.Link>
-        </Nav>
-        <Nav>
           <Nav.Link to="/about" as={NavLink}>About</Nav.Link>
         </Nav>
+        <Button 
+          style={{ width: "3rem", height: "3rem"}}
+          variant='outline-primary'
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-shopping-cart"><circle cx="9" cy="21" r="1"></circle><circle cx="20" cy="21" r="1"></circle><path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path></svg>
+        </Button>
       </Container>
     </NavbarBs>
   )
